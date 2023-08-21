428bd2f2da7b24a3408eed3eda0be36147aff4fd
asad-ch982 <asad.fakhar982@gmail.com>
2023-08-21 20:36:35 +0500

    21-08-2023-t

--- index.js
+++ index.js
@@ -174,7 +174,10 @@ app.post("/cusinvoice",jsonParser, async (req, res) => {
         const invoices = await Invoices.find({createdAt:{"$gt" : start+"T00:00:00.000Z","$lt" : end+"T23:59:59.000Z"}})
         const invoicesDetail = await InvoiceDetail.find({createdAt:{"$gt" : start+"T00:00:00.000Z","$lt" : end+"T23:59:59.000Z"}})
         console.log({invoices:invoices,invoiceDetailList:invoicesDetail})
-        res.status(200).json({invoices:invoices,invoiceDetailList:invoicesDetail})
+        if (invoices && invoicesDetail) {
+          res.status(200).json({invoices:invoices,invoiceDetailList:invoicesDetail})
+        }
+        
         
       } catch (error) {
           console.log(error)
